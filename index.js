9499eb8fa060e81fd0e43e464c0782e271b166ab
Elizabeth <113930754+ElizabethLankford@users.noreply.github.com>
2023-01-17 16:02:40 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const getBtn = document.getElementById("get-btn")
+const colorPicker = document.getElementById("color-picker")
+const colorScheme = document.getElementById("color-scheme")
+const colorPallet = document.querySelector(".pallet")
+const container = document.getElementById("container")
+
+let hexWithNoHash = '000000'
+let hex = ''
+let colorMode = ''
+
+colorPicker.addEventListener("change", function(){
+    hex = colorPicker.value
+    hexWithNoHash = hex.slice(1)
+})
+
+colorScheme.addEventListener("input", function(){
+    colorMode = colorScheme.value.toLowerCase()
+    
+})
+
+renderColorHtml()
+getBtn.addEventListener("click", renderColorHtml)
+
+function renderColorHtml(){
+    let html = ''
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hexWithNoHash}&mode=${colorMode}&count=5`)
+        .then((res) => res.json())
+        .then((data) => {
+        
+            data.colors.forEach(function (color){
+                let hexValue = color.hex.value
+                let nameValue = color.name.value
+
+                html += `
+                <div class="pallet" style="background-color:${hexValue}">
+                <div class="color-info">
+                <p class="hex">${hexValue}</p>
+                <p class="color-name">${nameValue}</p>
+                </div>
+                </div>
+
+            `
+            })
+            container.innerHTML = html
+        })
+       
+      
+       
+ }
